d18f92b3086e73ffa089cfc660e81db6d65aec1a
Emma Lee Belter <ebelter@purdue.edu>
2023-09-07 10:52:52 -0400

    commit

--- lab3belter.cpp
+++ lab3belter.cpp
@@ -2,23 +2,40 @@
 using namespace std;
 void printMenu(int& choice) {
 	cout << "Welcome to my program, please select an operation to perform:" << endl;
-	cout << "1. Placeholder" << endl;
-	cout << "2. Another Placeholder" << endl;
-	cout << "3. And a third one" << endl;
+	cout << "1. Add" << endl;
+	cout << "2. Subtract" << endl;
+	cout << "3. Multiply" << endl;
+	cout << "4. Divide" << endl;
 	cout << "\nYour Selection: ";
 	cin >> choice;
 }
 void getChoices(float& A, float& B) {
 	cout << "Please enter the first value:";
 	cin >> A;
+	cout << "Please enter the second value:";
+	cin >> B;
 	// The rest of this function is an exercise to the reader
 }
+
 void firstChoice(float A, float B) {
-	cout << "This is proving the first choice ran" << endl;
+	cout << A << "+" << B << "=" << A+B << endl;
+	//this function adds the first value (A) and second value (B)
 }
 void secondChoice(float A, float B) {
-	cout << "This is proving the second choice ran" << endl;
+	cout << A << "-" << B << "=" << A - B << endl;
+	//this function subtracts the first value (A) and second value (B)
 }
+void thirdChoice(float A, float B) {
+	int mult = A * B;
+	cout << A << "*" << B << "=" << mult << endl;
+	//this function multiplies the first value (A) and second value (B)
+}
+void forthChoice(float A, float B) {
+	int div = A / B;
+	cout << A << "/" << B << "=" << div << endl;
+	//this function divides the first value (A) and second value (B)
+}
+
 int main() {
 	int choice;
 	float A;
@@ -31,5 +48,12 @@ int main() {
 	if (choice == 2) {
 		secondChoice(A, B);
 	}
+	if (choice == 3) {
+		thirdChoice(A, B);
+	}
+	if (choice == 4) {
+		forthChoice(A, B);
+	}
+	//these if statements determine the function (void) needed to perform math operation choosen on A & B based on user input "choise"
 	return 0;
 }
